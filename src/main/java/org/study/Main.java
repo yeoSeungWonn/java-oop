8fff182407d870b306e2175444ff2dae93f6fd6e
yeoSeungWonn <dutmddnjs@hanmail.net>
2022-11-26 22:57:42 +0900

    Implement main entry point 실행될 메인 로직 작성 테스트 실행결과만 관측, 검증 코드는 없음

--- src/main/java/org/study/Main.java
+++ src/main/java/org/study/Main.java
@@ -1,7 +1,13 @@
 package org.study;
 
+import org.study.logic.BubbleSort;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BubbleSort<String> sort = new BubbleSort<>();
+
+        System.out.println("[result] " + sort.sort(Arrays.asList(args)));
     }
 }
---
+++ src/test/java/org/study/MainTest.java
@@ -0,0 +1,15 @@
+package org.study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        String[] args = {"2", "1", "3"};
+
+        Main.main(args);
+    }
+}
